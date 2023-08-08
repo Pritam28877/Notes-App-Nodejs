f9486915185a9db21832397432d9b55f2f0097bd
Pritam Das <dpritam28577@gmail.com>
2023-08-08 11:07:54 +0530

    add the all middelware setup to the index .js file

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
+require('dotenv').config();
 const express = require('express');
+const expressLayout = require("express-ejs-layouts");
 const app = express();
-const authRouth = require('./routes/auth')
-const Db = require("./config/mongoose")
+const port = 8000 || process.env.Port;
+const authRouth = require('./routes/auth');
+const Db = require("./config/mongoose");
 
 
 
+
+//middelware dependency 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//static file 
+app.use(express.static('public'));
+
+//templating engine
+app.use(expressLayout);
+app.set('layout' , './layouts/main');
+app.set('view engine','ejs');
 
 // route of the app
 app.use("/api/user", authRouth);
 
 
 
-app.listen(5000, (e) => {
-    if (e) {
-        console.log(e);
+app.listen(port, (error) => {
+    if (error) {
+        console.log(error);
     }
-    console.log(`app is running port 5000`)
+    console.log(`app is running port ${port}`);
 })
