6f1db8c6414775ce2c9f345b7e65107319b77d9d
Pritam Das <dpritam28577@gmail.com>
2023-08-09 10:46:15 +0530

    add the ejs of dashboard layout

--- controller/dashboardController.js
+++ controller/dashboardController.js
@@ -1,6 +1,10 @@
-module.exports.homepage = async(req ,res)=>{
-    const locals = {
-        title : "Dashboard",
-        description: " Free Node js notes apps ",
-    }
-}
+module.exports.homepage = async (req, res) => {
+  const locals = {
+    title: "Dashboard",
+    description: " Free Node js notes apps ",
+  };
+  res.render("dashboard/index", {
+    locals,
+    layout :"../views/layout/"
+  });
+};
