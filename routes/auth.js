112cc9695d41342839525e39fd828735180a88e4
Pritam Das <dpritam28577@gmail.com>
2023-08-12 12:39:03 +0530

    added the authenticate of google

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,36 @@
 const express = require("express");
 const route = express.Router();
 const passport = require("passport");
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://www.example.com/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.Client_ID,
+      clientSecret: process.env.Client_secret,
+      callbackURL: "http://localhost:8000/google/callback",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
+route.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
 
+route.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/login-failure",
+    successRedirect: "/dashboard",
+  })
+);
+
+route.get("/login-failure", (req, res) => {
+  res.send("something went wrong");
+});
 module.exports = route;
