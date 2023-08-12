fda315d7ff8dcb21891a7aa226d6758ad38fae14
Pritam Das <dpritam28577@gmail.com>
2023-08-12 12:24:04 +0530

    add the passport init

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const port = 8000 || process.env.Port;
 const homePage = require("./routes/index");
-const dashBoard = require('./routes/dashboard')
+const dashBoard = require("./routes/dashboard");
+const auth = require('./routes/auth')
 const Db = require("./config/mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const MongoStore = require("connect-mongo");
 
 //middelware dependency
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -21,7 +27,7 @@ app.set("view engine", "ejs");
 
 // route of the app
 app.use("/", homePage);
-app.use('/',dashBoard)
+app.use("/", dashBoard);
 app.get("*", (req, res) => {
   res.status(404).render("404");
 });
