a34de6b8a3931b1ca0709e7e4c89a01c4d9eccf9
Pritam Das <dpritam28577@gmail.com>
2023-08-14 18:08:52 +0530

    add the route and search ejs for the search function

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,7 +6,9 @@ const {
   dashboardupdateNotes,
   dashboardDeleteNote,
   dashboardAddNote,
-  dashboardAddNoteSubmit
+  dashboardAddNoteSubmit,
+  dashboardSearch,
+  dashboardSearchSubmit
 } = require("../controller/dashboardController");
 const { isLoggedIn } = require("../middleware/cheeckAuth");
 
@@ -14,7 +16,9 @@ const { isLoggedIn } = require("../middleware/cheeckAuth");
 route.get("/dashboard", isLoggedIn, homepage);
 route.get("/dashboard/item/:id", isLoggedIn, dashboardViewNotes);
 route.put("/dashboard/item/:id", isLoggedIn, dashboardupdateNotes);
-route.delete("/dashboard/item-delete/:id",isLoggedIn ,dashboardDeleteNote )
-route.get('/dashboard/add', isLoggedIn, dashboardAddNote);
-route.post('/dashboard/add', isLoggedIn, dashboardAddNoteSubmit);
+route.delete("/dashboard/item-delete/:id", isLoggedIn, dashboardDeleteNote);
+route.get("/dashboard/add", isLoggedIn, dashboardAddNote);
+route.post("/dashboard/add", isLoggedIn, dashboardAddNoteSubmit);
+route.get('/dashboard/search', isLoggedIn, dashboardSearch);
+route.post('/dashboard/search', isLoggedIn, dashboardSearchSubmit);
 module.exports = route;
