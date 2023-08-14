aeda0ee78a081fe7c7a068cf4a9c242e4ba0f642
Pritam Das <dpritam28577@gmail.com>
2023-08-14 13:06:24 +0530

    add the queary to adjust to the page of the dashboard

--- controller/dashboardController.js
+++ controller/dashboardController.js
@@ -1,12 +1,30 @@
-const Note = require('../models/Notes')
+const { default: mongoose } = require("mongoose");
+const Note = require("../models/Notes");
 
-module.exports.homepage = async (req, res) => {  
+module.exports.homepage = async (req, res) => {
+  let perPage = 12;
+  let page = req.query.page || 1;
   const locals = {
     title: "Dashboard",
     description: " Free Node js notes apps ",
   };
 
   try {
+    const Note = await Note.aggregate([
+      { $sort: { updatedAt: -1 } },
+      { $match: { user: mongoose.Types.ObjectId(req.user.id) } },
+      {
+        $project: {
+          title: { $substr: ["$title", 0, 30] },
+          body: { $substr: ["$body", 0, 100] },
+        },
+      }
+      ])
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec(); 
+
+
     const notes = await Note.find({});
     res.render("dashboard/index", {
       userName: req.user.firstName,
@@ -14,8 +32,5 @@ module.exports.homepage = async (req, res) => {
       locals,
       layout: "../views/layout/dashboard",
     });
-  } catch (error) {
-    
-  }
- 
+  } catch (error) {}
 };
