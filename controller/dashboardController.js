52bb1ce9418076fe8b3c79a1c0a96e19a5c388f3
Pritam Das <dpritam28577@gmail.com>
2023-08-14 17:13:23 +0530

    add the view note function

--- controller/dashboardController.js
+++ controller/dashboardController.js
@@ -2,6 +2,7 @@ const Note = require('../models/Notes');
 const { ObjectId } = require('mongodb');
 
 module.exports.homepage = async (req, res) => {
+
   const perPage = 12;
   const page = req.query.page || 1;
 
@@ -41,3 +42,22 @@ module.exports.homepage = async (req, res) => {
     res.status(500).send('An error occurred');
   }
 };
+module.exports.dashboardViewNotes = async (req, res) => {
+  const note = await Note.findById({ _id: req.params.id })
+    .where({ user: req.user.id })
+    .lean();
+
+  if (note) {
+    res.render("dashboard/view-note", {
+      noteID: req.params.id,
+      note,
+      layout: "../views/layouts/dashboard",
+    });
+  } else {
+    res.send("Something went wrong.");
+  }
+}
+
+module.exports.dashboardupdateNotes = async (req, res) => {
+
+}
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const route = express.Router();
-const { homepage } = require("../controller/dashboardController");
-const {isLoggedIn} = require('../middleware/cheeckAuth')
-
+const {
+  homepage,
+  dashboardViewNotes,
+  dashboardupdateNotes,
+} = require("../controller/dashboardController");
+const { isLoggedIn } = require("../middleware/cheeckAuth");
 
 //the route for the dashboard
-route.get("/dashboard",isLoggedIn , homepage);
+route.get("/dashboard", isLoggedIn, homepage);
+route.get("/dashboard/items/:id", isLoggedIn, dashboardViewNotes);
+route.post("/dashboard/items/:id", isLoggedIn, dashboardupdateNotes);
+
 module.exports = route;
